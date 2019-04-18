04210ae7d809d0943497ad09e0380be40e8c905a
Louis Magdaleno <buildthatapp@gmail.com>
2019-04-18 14:33:11 -0700

    Wired up index.js with redux

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Root from './Root';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import logger from 'redux-logger';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+// needed dependancies
+import rootReducer from './Reducer';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// applyMiddleware from redux
+// thunk from redux-thunk
+// logger from redux-logger
+// rootReducer from ./reducers
+
+const composeEnhancers =
+	typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(
+				{
+					// Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+				}
+			)
+		: compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+const store = createStore(rootReducer, enhancer);
+
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
